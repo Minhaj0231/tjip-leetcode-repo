23c308ca0171fdbee2c3b1910f3fecd4273b7ffe
Minhaj0231 <minhaj0231@gmail.com>
2022-04-16 01:30:50 +0600

    added week2 problem sollutions

---
+++ week02/4.valid_parentheses.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        brackets_dict  = {
+            ")":"(",
+            "}" :"{",
+            "]" : "[" 
+        }
+        stack = []
+        valid = True
+        for idx in range (0, len(s)):
+           
+            if s[idx]  in brackets_dict:
+                if len(stack) <= 0 or stack[len(stack)-1] != brackets_dict[s[idx]]:
+                    valid = False
+                    break
+                else:
+                    stack.pop()
+                    
+            else: 
+                stack.append(s[idx])
+                
+        if len(stack)>0 or not valid:
+            return False
+        
+        return True
